31d183bd79c7d01058ef953ed3e5d9336398be14
Vlada04 <v.omelchenko@ucu.edu.ua>
2021-02-16 02:28:18 +0200

    Limited number of markers on map

--- html_create.py
+++ html_create.py
@@ -2,17 +2,14 @@ import webbrowser
 import folium
 import pandas
 from geopy.geocoders import Nominatim
+from math import radians, cos, sin, asin, sqrt
 
 def read_file():
-    '''
-    
-    '''
     lst = []
     file = open('locations1.txt', 'r', encoding='utf-16')
     for line in file:
         lst.append(line)
     return lst
-
 
 def create_map_html():
     year = input("Please enter a year you would like to have a map for:")
@@ -20,9 +17,9 @@ def create_map_html():
     print("Map is generating...")
     print("Please wait...")
 
-
     loc = []
     loc.append(location.split(', '))
+
 
     map = folium.Map(location=[float(loc[0][0]), float(loc[0][1])])
 
@@ -49,11 +46,52 @@ def create_map_html():
         location = geolocator.geocode(i[-1])
         lst4.append([i[0], [location.latitude, location.longitude]])
 
+    lst5 = []
+    lst6 = []
+    lst7 = []
+    lst8 = []
+    list_of_markers = []
+    R = 6372.8
+    lat1 = float(loc[0][0])
+    lon1 = float(loc[0][1])
+
     for i in lst4:
+        dLat = radians(i[1][0] - lat1)
+        dLon = radians(i[1][1] - lon1)
+        lat1 = radians(lat1)
+        lat2 = radians(i[1][0])
+
+        a = sin(dLat/2)**2 + cos(lat1)*cos(lat2)*sin(dLon/2)**2
+        c = 2*asin(sqrt(a))
+        lst5.append(R*c)
+
+    H = []
+    j = 0
+    for i in lst4:
+        lst6.append(i[1])
+    while j != lst5.index(lst5[-1]):
+        H.append([lst5[j],lst6[j]])
+        j += 1
+
+
+    for i in lst5:
+        while len(lst7) != 5:
+            lst7.append(min(lst5))
+            lst5.remove(min(lst5))
+    
+    for i in lst7:
+        for k in H:
+            if i in k:
+                lst8.append(k[1])
+    for i in lst8:
+        for k in lst4:
+            if i in k:
+                list_of_markers.append(k)
+                
+    for i in list_of_markers:
         map.add_child(folium.Marker(location=i[-1],
                         popup=i[0],
                         icon=folium.Icon()))
     map.save('Map_5.html')
     return webbrowser.open('Map_5.html', new=2)
-
 print(create_map_html())
