44c93d477e97818b450824cc20aac26ded071848
Vlada04 <v.omelchenko@ucu.edu.ua>
2021-02-16 16:44:02 +0200

    Last changes

--- html_create.py
+++ html_create.py
@@ -29,38 +29,38 @@ def create_map_html():
     map = folium.Map(location=[float(loc[0][0]), float(loc[0][1])])
 
     lst = []
-    lst_of_movies = []
+    lst1 = []
     lst2 = []
-    sorted_lst_of_movies = []
-    list_with_coordin = []
+    lst3 = []
+    lst4 = []
     for i in read_file():
         if year in i:
             lst.append(i.split('\t'))
     for i in lst:
         if "(" not in i[-1] and "�" not in i[-1]:
-            lst_of_movies.append([i[0], i[-1][:-1]])
-    for i in lst_of_movies:
+            lst1.append([i[0], i[-1][:-1]])
+    for i in lst1:
         if len(i[-1].split(' ')) > 3:
             if [i[0].split('(')[0], i[-1].split(' ')[-2:]] not in lst2:
                 lst2.append([i[0].split('(')[0], i[-1].split(' ')[-2:]])
     for i in lst2:
-        sorted_lst_of_movies.append([i[0], i[1][0] + " " + i[1][1]])
+        lst3.append([i[0], i[1][0] + " " + i[1][1]])
 
-    for i in sorted_lst_of_movies:
+    for i in lst3:
         geolocator = Nominatim(user_agent="test")
         location = geolocator.geocode(i[-1])
-        list_with_coordin.append([i[0], [location.latitude, location.longitude]])
+        lst4.append([i[0], [location.latitude, location.longitude]])
 
-    lst_of_len = []
+    lst5 = []
     lst6 = []
-    limited_list = []
+    lst7 = []
     lst8 = []
     list_of_markers = []
     R = 6372.8
     lat1 = float(loc[0][0])
     lon1 = float(loc[0][1])
 
-    for i in list_with_coordin:
+    for i in lst4:
         dLat = radians(i[1][0] - lat1)
         dLon = radians(i[1][1] - lon1)
         lat1 = radians(lat1)
@@ -68,31 +68,31 @@ def create_map_html():
 
         a = sin(dLat/2)**2 + cos(lat1)*cos(lat2)*sin(dLon/2)**2
         c = 2*asin(sqrt(a))
-        lst_of_len.append(R*c)
+        lst5.append(R*c)
 
-    zipped_lst = []
+    H = []
     j = 0
-    for i in list_with_coordin:
+    for i in lst4:
         lst6.append(i[1])
-    while j != lst_of_len.index(lst_of_len[-1]):
-        zipped_lst.append([lst_of_len[j],lst6[j]])
+    while j != lst5.index(lst5[-1]):
+        H.append([lst5[j],lst6[j]])
         j += 1
 
 
-    for i in lst_of_len:
-        while len(limited_list) != 5:
-            limited_list.append(min(lst_of_len))
-            lst_of_len.remove(min(lst_of_len))
+    for i in lst5:
+        while len(lst7) != 5:
+            lst7.append(min(lst5))
+            lst5.remove(min(lst5))
     
-    for i in limited_list:
-        for k in zipped_lst:
+    for i in lst7:
+        for k in H:
             if i in k:
                 lst8.append(k[1])
     for i in lst8:
-        for k in list_with_coordin:
+        for k in lst4:
             if i in k:
                 list_of_markers.append(k)
-
+                
     for i in list_of_markers:
         map.add_child(folium.Marker(location=i[-1],
                         popup=i[0],
